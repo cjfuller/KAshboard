0b3c0b3b6a3daf662d68d6c3dcb01783a9c1ffd2
Colin J. Fuller <colin@khanacademy.org>
2014-07-18 11:41:14 -0400

    add RCSS

--- js/main.jsx
+++ js/main.jsx
@@ -5,6 +5,9 @@ window.React = React;
 
 var Dashboard = require("./dashboard.jsx");
 
+var RCSS = require("rcss");
+RCSS.injectAll();
+
 React.renderComponent(
     <Dashboard />,
     document.getElementById('content')
