d857cdcef5e2134da66df2070522ee2c5a7fba87
Colin J. Fuller <colin@khanacademy.org>
2014-07-18 20:50:23 -0400

    Fix widget sizes to actually fit a 5x3 grid.

--- js/style/widget-positioner.js
+++ js/style/widget-positioner.js
@@ -5,30 +5,33 @@ var styleVars = require("./style-vars.js");
 
 var gridSpacing = 15;
 var nWide = 5;
-var baseComponentSize = (styleVars.dashboardWidthPx - (nWide + 1) *
-                         gridSpacing)/nWide;
+var nTall = 3;
+var baseComponentWidth = (styleVars.dashboardWidthPx - (nWide + 1) *
+                          gridSpacing)/nWide;
+var baseComponentHeight = (styleVars.dashboardHeightPx - (nTall + 1) *
+                          gridSpacing)/nTall;
 
 var widgetStyleBase = {
     position: 'relative',
     marginTop: gridSpacing + 'px',
     marginLeft: gridSpacing + 'px',
     float: 'left',
-    width: baseComponentSize + 'px',
-    height: baseComponentSize + 'px',
+    width: baseComponentWidth + 'px',
+    height: baseComponentHeight + 'px',
     overflow: 'hidden',
 }
 
 var doubleWideWidget = _.defaults({
-    width: (2*baseComponentSize + gridSpacing) + 'px',
+    width: (2*baseComponentWidth + gridSpacing) + 'px',
 }, widgetStyleBase);
 
 var doubleTallWidget = _.defaults({
-    height: (2*baseComponentSize + gridSpacing) + 'px',
+    height: (2*baseComponentHeight + gridSpacing) + 'px',
 }, widgetStyleBase);
 
 var doubleDoubleWidget = _.defaults({
-    width: (2*baseComponentSize + gridSpacing) + 'px',
-    height: (2*baseComponentSize + gridSpacing) + 'px',
+    width: (2*baseComponentWidth + gridSpacing) + 'px',
+    height: (2*baseComponentHeight + gridSpacing) + 'px',
 }, widgetStyleBase);
 
 module.exports = {
