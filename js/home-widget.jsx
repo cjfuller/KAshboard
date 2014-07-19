86f26cecf171f263ed7564dc9b21e034dd88e577
Colin J. Fuller <colin@khanacademy.org>
2014-07-18 23:49:23 -0400

    more home widget WIP

--- js/home-widget.jsx
+++ js/home-widget.jsx
@@ -1,20 +1,84 @@
 var React = require("react");
 var RCSS = require("rcss");
+var moment = require("moment-timezone");
 
 var colors = require("./style/ka-colors.js");
 var WidgetContainer = require("./widget-container.jsx");
 
 var styles = require("./style/home-style.js");
 
+
+var TimeDateWidget = React.createClass({
+    updateTimeMs: 1000,
+    getInitialState: function() {
+        return {date: moment().tz(this.props.tz)};
+    },
+    getDateInTZ: function() {
+        this.setState({date: moment().tz(this.props.tz)});
+    },
+    render: function() {
+        var date = this.state.date;
+        return <div>
+            <div className={styles.date.className}>
+                {date.format("dddd MMMM DD, YYYY")}
+            </div>
+            <div className={styles.time.className}>
+                {date.format("h:mm:ss A")}
+            </div>
+        </div>
+    },
+    componentDidMount: function() {
+        this.interval = setInterval(this.getDateInTZ, this.updateTimeMs);
+    },
+});
+
+var WeatherWidget = React.createClass({
+
+    //dangerously setting weather font characters from a lookup table
+    //TODO(colin): is this actually safe if this is happening client-side?
+    weatherCharacterLookup: {
+        sunny: "&#xf00d;",
+        spacer: "&nbsp;&nbsp;"
+    },
+
+    render: function() {
+        var weather = this.weatherCharacterLookup;
+        return <div className={styles.weatherContainer.className}>
+            <span className={styles.weather.className}
+                 dangerouslySetInnerHTML={
+                     {__html: (weather[this.props.weather] + weather.spacer)}
+                 }/>
+            <span className={styles.temperature.className}>
+                {this.props.temp}
+                <span dangerouslySetInnerHTML={{__html: "&deg;"}}/>
+                {this.props.degreeType}
+            </span>
+        </div>
+    }
+});
+
+var LocationWidget = React.createClass({
+    render: function() {
+        return <div className={styles.locationHolder.className}>
+            <div className={styles.title.className}>Mountain View</div>
+            <TimeDateWidget tz="America/Los_Angeles"/>
+            <WeatherWidget temp="70" degreeType="F" weather='sunny'/>
+        </div>
+    },
+});
+
+
+
 var HomeWidget = React.createClass({
     render: function() {
         return <WidgetContainer sizeClass="doubleWide" color={colors.kaGreen}>
-            <div className={styles.className}>
-                <img alt="KA logo" 
+            <div className={styles.imgHolder.className}>
+                <img alt="KA logo"
                     src="/images/khan-logo-vertical-transparent.png"/>
             </div>
+            <LocationWidget className={styles.locationWidget.className}/>
         </WidgetContainer>
-    }
+    },
 });
 
 module.exports = HomeWidget;
--- js/style/home-style.js
+++ js/style/home-style.js
@@ -1,12 +1,58 @@
+var _ = require("underscore");
 var RCSS = require("rcss");
 var colors = require("./ka-colors.js");
 var styleVars = require("./style-vars.js");
-
 var logoHeightPx = 250;
 
-var imgDivStyle = {
-    paddingTop: ((styleVars.baseComponentHeight() - logoHeightPx)/2) + "px",
-    paddingLeft: "25px",
+var imgHolderStyle = {
+    marginTop: ((styleVars.baseComponentHeight() - logoHeightPx)/2) + "px",
+    marginLeft: "25px",
+    width: "auto",
+    float: "left",
+};
+
+var locationWidgetStyle = {
+};
+
+var locationHolderStyle = {
+    paddingLeft: "100px",
+    paddingTop: "40px",
+    float: "left",
+};
+
+var titleStyle = {
+    fontSize: "3.4em",
+    fontWeight: "bold",
+};
+
+var weatherContainer = {
+    marginTop: "15px",
 }
 
-module.exports = RCSS.registerClass(imgDivStyle);
+var weatherStyle = {
+    fontFamily: "weathericons",
+    fontSize: "3em",
+}
+
+var dateStyle = {
+    fontSize: "1.5em",
+}
+var timeStyle = {
+    fontSize: "3em",
+}
+
+var temperatureStyle = {
+    fontSize: "3em",
+}
+
+module.exports = {
+    imgHolder: RCSS.registerClass(imgHolderStyle),
+    locationWidget: RCSS.registerClass(locationWidgetStyle),
+    locationHolder: RCSS.registerClass(locationHolderStyle),
+    title: RCSS.registerClass(titleStyle),
+    date: RCSS.registerClass(dateStyle),
+    weather: RCSS.registerClass(weatherStyle),
+    weatherContainer: RCSS.registerClass(weatherContainer),
+    temperature: RCSS.registerClass(temperatureStyle),
+    time: RCSS.registerClass(timeStyle),
+};
