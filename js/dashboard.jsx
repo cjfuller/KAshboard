969f5260a4e58321d5cb23045b79dc847376ffc3
Tony Liu <tony@khanacademy.org>
2014-07-19 20:48:56 -0700

    Update dashboard references

--- js/dashboard.jsx
+++ js/dashboard.jsx
@@ -7,7 +7,7 @@ var GAWidget = require("./ga-widget.jsx");
 var StoriesWidget = require("./stories-widget.jsx");
 var GHMetaWidget = require("./github-widget.jsx");
 var TeamWidget = require("./team-widget.jsx");
-var BigQueryWidget = require("./bq-widget.jsx");
+var ExperimentsWidget = require("./experiments-widget.jsx");
 var WidgetContainer = require("./widget-container.jsx");
 var AtAllWidget = require("./at-all-widget.jsx");
 var HomeWidget = require("./home-widget.jsx");
@@ -26,7 +26,7 @@ var Dashboard = React.createClass({
             <ErrorGraphWidget />
             <TeamWidget />
             <AtAllWidget />
-            <BigQueryWidget />
+            <ExperimentsWidget />
             <RegistrationsGraphWidget />
             <RegistrationsWidget />
         </div>;
